aac3fecd8d20167ad243624cc3923d53421dd551
Haren-Sharma <harensharma33@gmail.com>
2024-10-22 13:32:17 +0530

    products crud

--- api/src/db/index.ts
+++ api/src/db/index.ts
@@ -1,7 +1,7 @@
-import { Pool } from "pg";
+import pg from "pg";
 import { drizzle } from "drizzle-orm/node-postgres";
 
-const pool = new Pool({
+const pool = new pg.Pool({
   connectionString: process.env.DATABASE_URL!,
   //! operator to assure TypeScript that process.env.DATABASE_URL is definitely defined at runtime, 
   //even though the TypeScript type checker might otherwise flag it as possibly null or undefined.
